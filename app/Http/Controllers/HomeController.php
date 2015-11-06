e01af495aac969f6e6975fbf24378339a32c8fa1
raka aditya <adm@rakaaditya.com>
2015-11-06 18:51:19 +0700

    Using redis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,27 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Votes;
 use DB;
+use Redis;
+const COUNTER_KEY = 'MENDOAN:COUNT';
 
 class HomeController extends Controller
 {
+    
+    function __construct()
+    {
+         $this->redis    = new \Predis\Client();
+    }
+
     public function index(Request $request)
     {
+        if(! $count = $this->redis->get(COUNTER_KEY)) {
+            $count = Votes::count();
+            $this->redis->set(COUNTER_KEY, $count);
+        }
+
         $total = str_pad(Votes::count(), 6, 0, STR_PAD_LEFT);
         $exist = Votes::where('ip_address', $request->getClientIp())->count();
+        
         return view('home', ['total' => $total, 'exist' => $exist]);
     }
 
@@ -26,12 +40,19 @@ class HomeController extends Controller
                 'error'     => $validator->errors(),
             ];
         } else {
+            if(! $count = $this->redis->get(COUNTER_KEY))
+                $count = Votes::count();
+            
             $data = Votes::firstOrCreate([
                 'ip_address'  => $request->getClientIp()
             ]);
             $data->save();
-
-            $total = str_pad(Votes::count(), 6, 0, STR_PAD_LEFT);
+            
+            
+            $count = (int)$count+1;
+            $this->redis->set(COUNTER_KEY, $count);
+            $total = str_pad($count, 6, 0, STR_PAD_LEFT);
+            
             echo $total;
         }
     }
