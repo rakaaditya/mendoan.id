0bc22fa1ecaacb9dd47c315f1a2921a91e147a25
raka aditya <adm@rakaaditya.com>
2015-11-07 13:08:24 +0700

    Add K style count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,10 +24,23 @@ class HomeController extends Controller
             $this->redis->set(COUNTER_KEY, $count);
         }
 
+        if ($count > 999 && $count <= 999999) {
+            $kFormat = number_format($count / 1000, 1, '.', ' ').' K'; 
+        } elseif ($count > 999999) {
+            $kFormat = number_format($count / 1000000, 1, '.', ' ').' M';
+        } else {
+            $kFormat = $count;
+        }
+
         $total = str_pad($count, 6, 0, STR_PAD_LEFT);
         $exist = Votes::where('ip_address', $request->getClientIp())->count();
         
-        return view('home', ['total' => $total, 'count' => $count, 'exist' => $exist]);
+        return view('home', [
+            'total'     => $total, 
+            'count'     => $count, 
+            'exist'     => $exist,
+            'kFormat'   => $kFormat
+        ]);
     }
 
     public function store(Request $request)
