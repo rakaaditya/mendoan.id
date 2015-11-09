603cc2c40d923e6dc688d46cffd2c75b1d0810fe
raka aditya <adm@rakaaditya.com>
2015-11-09 13:54:29 +0700

    Add credits

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,12 +12,27 @@
                 <div class="col-md-12 col-xs-12 about">
                     <p>Aplikasi ini dibuat awalnya hanya untuk keisengan semata. Bukan serius membuat petisi untuk menggugat. Hal ini sebagai klarifikasi atas pemberitaan media yang berkembang sekarang. Dan sama sekali tidak ada maksud untuk membuat suasana semakin panas.</p>
                     <p>Beberapa hari yang lalu, ketika mulai ramai tentang masalah privatisasi "Mendoan", saya dan seorang senior saya, Mas <a href="http://soep.web.id/" target="_blank">Soep</a>, yang juga pegiat Gerakan Desa Membangun secara singkat mengeluarkan ide untuk membeli domain <a href="http://www.mendoan.id">mendoan.id</a>. Setelah domain di tangan kami, barulah terpikir mau diapakan domain ini. Akhirnya saya mencoba membuat sebuah aplikasi kecil yang sangat sederhana dengan sebuat tombol untuk menyatakan mendukung menolak privatisasi mendoan. Saya sampaikan ide saya ke Mas Soep dan aplikasi siap digunakan pada hari Jumat (6/11) lalu tepatnya sekitar pukul 18.00.</p>
-                	<p>Tidak disangka, dengan bantuan dari komunitas, akun twitter @iniPurwokerto, serta teman-teman lainnya yang begitu antusias menyebarkan website ini, dalam 2 jam sudah ada 800-an orang yang mendukung dengan mengeklik sebuah tombol kecil di yang saya buat hanya dalam hitungan menit sambil ngemil kuaci di kantor.</p>
+                	<p>Tidak disangka, dengan bantuan dari komunitas, akun twitter @iniPurwokerto, Mas <a href="https://twitter.com/sigitwid/" target="_blank">Sigit Widodo</a>, serta teman-teman lainnya yang begitu antusias menyebarkan website ini, dalam 2 jam sudah ada 800-an orang yang mendukung dengan mengeklik sebuah tombol kecil di yang saya buat hanya dalam hitungan menit sambil ngemil kuaci di kantor.</p>
                 	<p>Selang beberapa jam, angka terus bertambah. Hingga mendekati jam 11 malam, counter menembus angka 1000. Saat itu saya mulai memikirkan antisipasi jumlah pengunjung yang semakin melonjak. Aplikasi yang tadinya saya buat sambil duduk santai, mulai saya perhatikan serius. Mulai dari optimalisasi cache, sampai modifikasi css yang tadinya hanya menggunakan bootstrap dengan tambahan 1 line untuk ukuran font judul. Karena saya terlalu lemah dalam urusan desain, saya meminta bantuan dibuatkan logo ke <a href="https://twitter.com/uungferi/" target="_blank">Mas Uung</a> agar terlihat lebih serius</p>
                 	<p>Sampai pada hari kedua, Mas Uung "Request" ke saya untuk menambah kolom komentar untuk mengukur pendapat orang-orang. Ditambah dengan Mas Soep yang ikut berpendapat untuk ditambah kolom nama. Semakin besar nih aplikasi. Saya langsung buatkan malam itu juga. Dan baru beberapa jam dibuat, sebuah kolom di paling bawah tempat menampung komentar langsung penuh dengan komentar-komentar masyarakat. Mulai dari yang pro sampai yang kontra. Mulai dari yang berbahasa halus, sampai yang rasis. Saya sempat merasa bersalah membuat kolom komentar ini. Tapi ini pendapat masyarakat. Tidak ada fitur moderasi di komentar ini. Biarkan suara mereka mengalir di sebuah <i>timeline</i> kolom komentar ini.</p>
                 	<p>Sekali lagi, saya membuat aplikasi ini bukan untuk membuat suasana semakin panas. Sekarang aplikasi ini menjadi fasilitas masyarakat untuk menyampaikan pendapatnya tentang privatisasi "Mendoan".</p>
                 	<p><br/><br/><br/></p>
                 	<p align="right">Warm Regards,<br/><br/><a href="http://www.rakaaditya.com/" target="_blank">Raka Aditya</a><br/>Developer</p>
+                    <hr/>
+                    <p><b>Mendoan.id dikelola (bukan dimiliki) bersama berkat kolaborasi dari:</b></p>
+                    <b>Publikasi:</b>
+                    <ul>
+                        <li>Sigit Widodo</li>
+                        <li>Agung</li>
+                    </ul>
+                    <b>Teknis:</b>
+                    <ul>
+                        <li>Raka Aditya (Web Developer)</li>
+                        <li>Supriyanto (Domain Registrant)</li>
+                        <li>Kris Fajar (Server Provider)</li>
+                        <li>Uung Feri (Desain Logo)</li>
+                        <li>Uung Feri (Desain Grafis)</li>
+                    </ul>
                 </div>
             </div>
         </div>
